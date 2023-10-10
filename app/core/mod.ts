c2e55b50d261b360f8585750ba45ace606f630fb
toridoriv <vrodriguezfe@icloud.com>
2023-10-10 17:51:54 -0300

    :building_construction: (server): Load middlewares dynamically

--- app/core/mod.ts
+++ app/core/mod.ts
@@ -3,3 +3,4 @@ export * from "./fanfiction.ts";
 export * from "./logger.ts";
 export * from "./endpoints/endpoint.module.ts";
 export * from "./tag.ts";
+export * from "./utils.ts";
--- app/server/server.module.ts
+++ app/server/server.module.ts
@@ -1,4 +1,4 @@
-import { express } from "@deps";
+import { express, type mongodb } from "@deps";
 import {
   createLogger,
   DatabaseClient,
@@ -10,6 +10,7 @@ import * as middleware from "./server.middlewares.ts";
 import AssetsRouter from "./routers/router.assets.ts";
 import WebpageRouter from "./routers/router.webpage.ts";
 import ApiRouter from "./routers/router.api.ts";
+import { retrieveMiddlewares } from "./server.utils.ts";
 
 const application = express();
 
@@ -19,6 +20,12 @@ application.logger = createLogger({
   severity: serverConfig.loggerSeverity,
   environment: serverConfig.environment,
   mode: serverConfig.loggerMode,
+  inspectOptions: {
+    colors: true,
+    showHidden: true,
+    showProxy: true,
+    getters: true,
+  },
 });
 
 const database = new DatabaseClient(serverConfig.dbUri, {
@@ -26,12 +33,23 @@ const database = new DatabaseClient(serverConfig.dbUri, {
 }).registerCollection<FanfictionDocumentOutput, "fanfictions">("fanfictions");
 
 application.use(middleware.inspectRequest);
+application.use(...(await retrieveMiddlewares()));
+
+application.disable("x-powered-by");
 application.engine(".hbs", middleware.handlebars.engine);
 application.set("view engine", ".hbs");
 application.set("views", "./app/views");
+
+application.locals.title = "Duofiction";
+application.locals.config = serverConfig;
+
+application.use(express.urlencoded({ extended: true }));
+application.use(express.json());
 application.use(AssetsRouter);
 application.use(WebpageRouter);
 application.use("/api", ApiRouter);
+
+await database.connect();
 
 const server = application.listen(serverConfig.port, handleListening);
 
@@ -40,9 +58,7 @@ server.on("close", handleClose);
 Deno.addSignalListener("SIGTERM", handleKillSignal);
 Deno.addSignalListener("SIGINT", handleKillSignal);
 
-async function handleListening() {
-  await database.connect();
-
+function handleListening() {
   application.fanfics = database.fanfictions;
   application.logger.info(`Listening on port ${serverConfig.port}.`);
 }
@@ -60,10 +76,20 @@ async function handleKillSignal() {
 }
 
 declare global {
+  namespace Application {
+    type FanfictionsCollection = mongodb.Collection<FanfictionDocumentOutput>;
+
+    type FindFanfictionOptions = mongodb.FindOptions<FanfictionDocumentOutput>;
+  }
+
   namespace Express {
     interface Application {
       fanfics: (typeof database)["fanfictions"];
       logger: Logger;
+    }
+
+    interface Locals {
+      config: typeof serverConfig;
     }
   }
 }
