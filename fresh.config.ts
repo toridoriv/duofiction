1e101643fcbb48be27ce3e55d5bc385b76619d3d
toridoriv <vrodriguezfe@icloud.com>
2023-10-19 09:59:46 -0300

    :technologist: Improve code related to shutting down the local server

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "$fresh/server.ts";
-import { MainAbortController } from "@utils/mod.ts";
+import {
+  logger,
+  MainAbortController,
+  shutdownApplication,
+} from "@utils/mod.ts";
 
 export default defineConfig({
   server: {
     port: 3000,
     signal: MainAbortController.signal,
+    onListen(params) {
+      logger.info(
+        `Server listening on: http://${params.hostname}:${params.port}`,
+      );
+      Deno.addSignalListener("SIGTERM", shutdownApplication);
+      Deno.addSignalListener("SIGINT", shutdownApplication);
+    },
   },
 });
--- utils/database.ts
+++ utils/database.ts
@@ -4,7 +4,6 @@ import { Database } from "@modules/database/mod.ts";
 import config from "@modules/config/mod.ts";
 import { logger } from "@utils/logger.ts";
 import { FanfictionOutput } from "@modules/fanfiction/mod.ts";
-import { MainAbortController } from "@utils/abort-signals.ts";
 
 export const client = new Database.Client(config.MONGODB_URI, {
   logger: logger.getSubLogger({ module: "database" }),
@@ -14,16 +13,4 @@ export const database = client.db();
 
 if (!client.isConnected) {
   await client.connect();
-}
-
-Deno.addSignalListener("SIGTERM", handleKillSignal);
-Deno.addSignalListener("SIGINT", handleKillSignal);
-
-async function handleKillSignal() {
-  if (client.isConnected) {
-    await client.close(false);
-  }
-
-  MainAbortController.abort("A kill signal has been received.");
-  Deno.exit(0);
 }
