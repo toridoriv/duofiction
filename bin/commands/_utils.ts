e06be8fb6b363ce81b49aa47751256c1fb8d79eb
toridoriv <vrodriguezfe@icloud.com>
2023-10-23 16:33:40 -0300

    :sparkles: Add command to generate lock file

--- bin/commands/_utils.ts
+++ bin/commands/_utils.ts
@@ -1,5 +1,5 @@
 import { Logger } from "@modules/logger/mod.ts";
-import { CliffyCommand, WalkOptions, walkSync } from "../deps.ts";
+import { CliffyCommand, existsSync, WalkOptions, walkSync } from "@bin/deps.ts";
 
 export const logger = Logger.create({
   severity: "DEBUG",
@@ -38,4 +38,68 @@ export async function registerCommands(
   }
 
   return commands;
+}
+
+export function getDependencyFilePaths(isDevelopment: boolean) {
+  const match: RegExp[] = [/deps\./, /\.d\.ts/];
+  const skip: RegExp[] = [/node_modules/];
+
+  if (isDevelopment) {
+    match.push(/prettier\.config/, /scripts\.config/);
+  } else {
+    skip.push(/bin\//);
+  }
+
+  const options: WalkOptions = {
+    includeDirs: false,
+    exts: [".ts"],
+    match,
+    skip,
+  };
+
+  return [...walkSync("./", options)].map((e) => e.path);
+}
+
+export function runDenoCache(writeLock: boolean, path: string) {
+  logger.info(`ℹ️  Caching dependencies for ./${path}`);
+  const args = ["cache"];
+
+  if (writeLock) {
+    args.push("--lock=deno.lock");
+  }
+
+  args.push(path);
+
+  const output = executeCommand("deno", {
+    args,
+  });
+
+  return output;
+}
+
+export function removePreviousCache() {
+  const denoDir = getCachePath();
+  const nodeModulesDir = "./node_modules";
+
+  if (existsSync(denoDir)) {
+    logger.info(`ℹ️  Deleting cache on ${denoDir}`);
+    executeCommand("rm", { args: ["-rf", denoDir] });
+  }
+
+  if (existsSync(nodeModulesDir)) {
+    logger.info(`ℹ️  Deleting cache on ${nodeModulesDir}`);
+    executeCommand("rm", { args: ["-rf", nodeModulesDir] });
+  }
+}
+
+export function getCachePath() {
+  const output = executeCommand("deno", { args: ["info", "--json"] });
+
+  return JSON.parse(output).denoDir as string;
+}
+
+export function removeOldLockFile() {
+  logger.info(`ℹ️  Deleting deno.lock file...`);
+
+  executeCommand("rm", { args: ["./deno.lock"] });
 }
--- bin/commands/cache.ts
+++ bin/commands/cache.ts
@@ -1,30 +1,34 @@
+import { CliffyCommand } from "@bin/deps.ts";
 import {
-  CliffyCommand,
-  existsSync,
-  type WalkOptions,
-  walkSync,
-} from "../deps.ts";
-import { executeCommand } from "./_utils.ts";
+  getDependencyFilePaths,
+  logger,
+  removePreviousCache,
+  runDenoCache,
+} from "@bin/commands/_utils.ts";
 
 const CacheCommand = new CliffyCommand.Command()
   .name("cache")
   .description("Cache all dependencies for this project.")
-  .option("-d, --development [development:boolean]", "", { default: false })
+  .option(
+    "-d, --development [development:boolean]",
+    "Include development dependencies.",
+    { default: false },
+  )
   .option(
     "-c --clean [clean:boolean]",
     "Use this option to remove all previous cache.",
     { default: false },
   )
   .action(function main(options) {
-    const paths = getDependencyFiles(options.development);
+    const paths = getDependencyFilePaths(options.development);
 
     if (options.clean) {
       removePreviousCache();
     }
 
-    paths.forEach(cacheDependencies.bind(null, options.development));
+    paths.forEach(runDenoCache.bind(null, options.development));
 
-    console.info("✅ All dependencies were cached.");
+    logger.info("✅ All dependencies were cached.");
   });
 
 if (import.meta.main) {
@@ -32,70 +36,3 @@ if (import.meta.main) {
 }
 
 export default CacheCommand;
-
-function getDependencyFiles(isDevelopment: boolean) {
-  const paths = [] as string[];
-  const skip = [/node_modules/, new RegExp("mod.ts")];
-  const opts: WalkOptions = {
-    includeDirs: false,
-    exts: [".ts"],
-    match: [
-      /deps\./,
-      new RegExp(".d.ts"),
-    ],
-    skip,
-  };
-
-  if (!isDevelopment) {
-    skip.push(/bin\//);
-  } else {
-    opts.exts?.push(".mjs");
-    opts.match?.push(
-      new RegExp("prettier.config.mjs"),
-      new RegExp("scripts.config.ts"),
-    );
-  }
-
-  for (const entry of walkSync("./", opts)) {
-    paths.push(entry.path);
-  }
-
-  return paths;
-}
-
-function removePreviousCache() {
-  const denoDir = getCacheFile();
-  const nodeModulesDir = "./node_modules";
-
-  if (existsSync(denoDir)) {
-    console.info(`ℹ️  Deleting cache on ${denoDir}`);
-    executeCommand("rm", { args: ["-rf", denoDir] });
-  }
-
-  if (existsSync(nodeModulesDir)) {
-    console.info(`ℹ️  Deleting cache on ${nodeModulesDir}`);
-    executeCommand("rm", { args: ["-rf", nodeModulesDir] });
-  }
-}
-
-function cacheDependencies(isDevelopment: boolean, path: string) {
-  console.info(`ℹ️  Caching dependencies for ./${path}`);
-  const args = ["cache"];
-
-  if (!isDevelopment) {
-    args.push("--lock=deno.lock");
-  }
-
-  args.push(path);
-  const output = executeCommand("deno", {
-    args,
-  });
-
-  return output;
-}
-
-function getCacheFile() {
-  const output = executeCommand("deno", { args: ["info", "--json"] });
-
-  return JSON.parse(output).denoDir as string;
-}
